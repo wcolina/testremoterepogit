e45cc638ec8df29683fa56db7a48a5d60f11a42f
wcolina <walker.cbdiv@gmail.com>
2018-04-24 01:48:58 -0400

    first commit

---
+++ src/main/java/com/wc/myprojforgit/MyprojforgitApplication.java
@@ -0,0 +1,12 @@
+package com.wc.myprojforgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojforgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyprojforgitApplication.class, args);
+    }
+}
